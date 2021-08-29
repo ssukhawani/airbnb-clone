865403a85de33ea73e64c2e6cbc9ff2fbe495372
Sahil Sukhwani <sahil.sukhwani@coditation.com>
2021-08-29 13:16:26 +0530

    added padding x 22 for medidium screen

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 py-14 bg-gray-100 px-10 sm:px-16 md:px-28 text-gray-600 gap-y-10 gap-x-6">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 py-14 bg-gray-100 px-10 sm:px-16 md:px-22 lg:px-28 text-gray-600 gap-y-10 gap-x-6">
             <div className="space-y-4 text-sm text-gray-800 "> 
                 <h5 className="font-bold">ABOUT</h5>
                 <p className="cursor-pointer">How Airbnb works?</p>
